01e1a157d0076b602240117b13782ece6899b793
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-09-26 20:17:05 +0530

    added images to slider

--- app/components/common/ImageCarousel.js
+++ app/components/common/ImageCarousel.js
@@ -1,6 +1,11 @@
 import React, { useRef, useState } from "react";
-import { Dimensions, Text, View } from "react-native";
+import { Dimensions, Image, Text, View } from "react-native";
 import Carousel from "react-native-snap-carousel";
+import banner1 from "../../assets/banners/banner1.jpg";
+import banner2 from "../../assets/banners/banner2.jpg";
+import banner3 from "../../assets/banners/banner3.jpg";
+import banner4 from "../../assets/banners/banner4.jpg";
+import banner5 from "../../assets/banners/banner5.png";
 
 const width = Dimensions.get("window")?.width - 20;
 
@@ -8,26 +13,31 @@ const carouselItems = [
   {
     title: "Item 111",
     text: "Text 1",
+    image: banner1,
   },
   {
     title: "Item 222",
     text: "Text 2",
+    image: banner2,
   },
   {
     title: "Item 333",
     text: "Text 3",
+    image: banner3,
   },
   {
     title: "Item 224",
     text: "Text 4",
+    image: banner4,
   },
   {
     title: "Item 544",
     text: "Text 5",
+    image: banner5,
   },
 ];
 
-const _renderItem = ({ item, index }) => {
+const _renderItem = ({ item }) => {
   return (
     <View
       style={{
@@ -42,6 +52,7 @@ const _renderItem = ({ item, index }) => {
     >
       <Text style={{ fontSize: 30, color: "blue" }}>{item.title}</Text>
       <Text style={{ fontSize: 30, color: "green" }}>{item.text}</Text>
+      <Image source={item.image} />
     </View>
   );
 };
@@ -61,6 +72,7 @@ const ImageCarousel = () => {
         ref={inputEl}
         onSnapToItem={(index) => setActiveIndex(index)}
       />
+      <Text>activeIndex: {activeIndex}</Text>
     </View>
   );
 };
